2dc62ec70d4348e35188763889164a868189ffb8
David Langham <dlangham@users.noreply.github.com>
2019-02-05 17:04:18 -0600

    fix gulpfile watcher and add plumber, create readme.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# D&D 5e Character Sheet
+
+* Dynamic character sheet
+* Mobile-first design
+* SPA
+* Built on Bootstrap and jQuery
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,16 @@
 const gulp =        require('gulp'),
       sass =        require('gulp-sass'),
       sourcemaps =  require('gulp-sourcemaps'),
+      plumber =     require('gulp-plumber'),
       browserSync = require('browser-sync').create(),
       reload =      browserSync.reload;
  
-// Compile sass, map source, then reload  [[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]
+// Compile sass, map source, then reload  -----------------------
 gulp.task('sass', function() {
     gulp.src('./app/styles/sass/**/*.scss')
+        .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass())
-        .on('error', function (err) {
-            console.log(err.toString());
-            this.emit('end');
-        })
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./app/styles/'))
         .pipe(reload({
@@ -20,8 +18,8 @@ gulp.task('sass', function() {
         }))
 });
 
-// Browsersync any file changes [[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]
-gulp.task('sync', function () {
+// Watchers and browsersync any file changes ----------------------------------
+gulp.task('sync', function() {
  
     // Serve files from the root of this project
     browserSync.init({
@@ -34,9 +32,9 @@ gulp.task('sync', function () {
     
     // Watchers
     gulp.watch('./app/**/*.+(html|js|php)', reload);
-    gulp.watch('./app/styles/sass/*.sass', ['sass']);
-
+    gulp.watch('./app/styles/sass/*.scss', ['sass']);
+    
 });
 
-// Use `gulp` in command line   [[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]
+// `gulp` command   ---------------------------------------
 gulp.task('default', ['sass', 'sync']);
